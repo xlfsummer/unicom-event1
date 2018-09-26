790ca980fb355b39a75f8d4d0d0cef8a8fcdbdda
xlfsummer <xlf-summer@outlook.com>
2018-09-27 00:31:58 +0800

    page 1 80%

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
   </div>
@@ -22,33 +18,12 @@ body {
 }
 
 #app {
+  min-height: 150vw;
+  height: 100vh;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
 
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
+  background: #fff url("./assets/bg1.png") top center/100% auto no-repeat;
 }
 </style>
---
+++ src/components/InputPage.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="page-input">
+        <form>
+            <input/>
+            <input/>
+            <select name="" id="">
+                <option v-for="(job, index) in jobList" :key="index" :value="job">{{job}}</option>
+            </select>
+            <!-- 立即生成承诺书 -->
+            <button class="submit" type="submit"></button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      jobList: [
+        '网络建设岗',
+        '一线服务岗',
+        '服务支撑岗',
+        '市场营销岗',
+        '行政服务岗'
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.page-input {
+  display: flex;
+  justify-content: center;
+}
+form {
+  margin-top: 83.8vw;
+  width: 70vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+input,
+select,
+button {
+  height: 7.8vw;
+  width: 47.5vw;
+  margin-left: 12.5vw;
+  padding-left: 2vw;
+  box-sizing: border-box;
+  margin-bottom: 3.6vw;
+  background: transparent;
+  border: none;
+}
+
+input {
+  /* border: 1px red solid; */
+}
+select {
+  appearance: none;
+}
+
+button.submit {
+  margin-top: 0.5vw;
+  height: 8.2vw;
+  margin-left: 0;
+  width: 60vw;
+  border: none;
+}
+</style>
